a7f5ec051034aef9531625707eed2efb8cf91ace
Denaun <mauzuc90@yahoo.it>
2019-12-01 08:14:30 +0100

    Initial day 1 commit.

---
+++ day_1/day_1/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Day 1."""
+
+__author__ = """Maurizio Zucchelli"""
+__email__ = 'mauzuc90@yahoo.it'
+__version__ = '0.1.0'
---
+++ day_1/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Day 1 in a project::
+
+    import day_1
