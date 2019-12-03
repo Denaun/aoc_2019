20d7689b3fe9d1e592e74e1080f713e4ce1da6d4
Denaun <mauzuc90@yahoo.it>
2019-12-03 21:48:42 +0100

    Initial day 3 commit.

---
+++ day_3/lib/day_3.rb
@@ -0,0 +1,6 @@
+require "day_3/version"
+
+module Day3
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ day_3/spec/day_3_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Day3 do
+  it "has a version number" do
+    expect(Day3::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
