fbd12313ad9700dfdf6fe2305cfd3c9f801c466e
Denaun <mauzuc90@yahoo.it>
2019-12-04 22:07:26 +0100

    Implement day 3 (part 1).
    Awful solution.

--- day_3/lib/day_3.rb
+++ day_3/lib/day_3.rb
@@ -1,6 +1,82 @@
-require "day_3/version"
+# frozen_string_literal: true
+
+require 'ostruct'
+require 'day_3/version'
 
 module Day3
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Point
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
+    attr_reader :x
+    attr_reader :y
+
+    def to_s
+      "(#{x}, #{y})"
+    end
+
+    def manhattan_length
+      @x.abs + @y.abs
+    end
+
+    def ==(other)
+      @x == other.x && @y == other.y
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      "#{x}-#{y}".hash
+    end
+  end
+
+  def self.parse(str)
+    segment = OpenStruct.new
+    segment.direction = case str[0]
+                        when 'R'
+                          :right
+                        when 'L'
+                          :left
+                        when 'U'
+                          :up
+                        when 'D'
+                          :down
+                        else
+                          raise Error, "Unknown character #{dir}"
+    end
+    segment.length = str[1..-1].to_i
+    segment
+  end
+
+  def self.to_points(segments)
+    start = Point.new(0, 0)
+    points = []
+    segments.each do |s|
+      values = (1..s.length)
+      points += case s.direction
+                when :right
+                  values.collect { |v| Point.new(start.x + v, start.y) }
+                when :left
+                  values.collect { |v| Point.new(start.x - v, start.y) }
+                when :up
+                  values.collect { |v| Point.new(start.x, start.y + v) }
+                when :down
+                  values.collect { |v| Point.new(start.x, start.y - v) }
+        end
+      start = points.last
+    end
+    points.to_set
+  end
+
+  def self.solve(wire1, wire2)
+    (to_points(wire1.split(',').collect { |x| parse(x) }) &
+     to_points(wire2.split(',').collect { |x| parse(x) }))
+      .collect(&:manhattan_length).to_a.min
+  end
 end
--- day_3/spec/day_3_spec.rb
+++ day_3/spec/day_3_spec.rb
@@ -1,9 +1,27 @@
+# frozen_string_literal: true
+
 RSpec.describe Day3 do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Day3::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'calculates Manhattan length correctly' do
+    expect(Day3::Point.new(0, 0).manhattan_length).to eq(0)
+    expect(Day3::Point.new(2, 0).manhattan_length).to eq(2)
+    expect(Day3::Point.new(0, 2).manhattan_length).to eq(2)
+    expect(Day3::Point.new(3, 4).manhattan_length).to eq(7)
+    expect(Day3::Point.new(-3, -4).manhattan_length).to eq(7)
+  end
+
+  it 'solves the examples' do
+    expect(Day3.solve('R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                      'U62,R66,U55,R34,D71,R55,D58,R83')).to eq(159)
+    expect(Day3.solve('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+                      'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7')).to eq(135)
+  end
+
+  it 'solves part 1' do
+    puzzle_input = File.open(File.join(File.dirname(__FILE__), 'input')).collect.to_a
+    expect(Day3.solve(puzzle_input[0], puzzle_input[1])).to eq(721)
   end
 end
