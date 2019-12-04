d54fe552825a7e155d6a93a5ed6827d3e8b2656c
Denaun <mauzuc90@yahoo.it>
2019-12-05 00:11:33 +0100

    Implement day 4 (part 2).

--- day_4/src/main/scala/day4/Password.scala
+++ day_4/src/main/scala/day4/Password.scala
@@ -1,22 +1,38 @@
 package day4
 
 object Password {
-  def isValid(pass: String): Boolean = {
+  def isValid(pass: String)(implicit part1: Boolean=false): Boolean = {
     if (pass.length() != 6) {
       return false
     }
-    val stream = pass.map { c =>
-      c.asDigit
+    val stream = pass.map(_.asDigit)
+    if (part1) {
+      if (stream
+            .sliding(2)
+            .find { d =>
+              d(0) == d(1)
+            }
+            .isEmpty) {
+        return false
+      }
+    } else {
+      if (stream
+            .prepended(Int.MinValue) // Invalid bound values
+            .appended(Int.MinValue)
+            .sliding(4)
+            .find { d =>
+              d(0) != d(1) && d(1) == d(2) && d(2) != d(3)
+            }
+            .isEmpty) {
+        return false
+      }
     }
-    val adjacent = stream.zip(stream.tail)
-    if (adjacent.find { d =>
-          d._1 == d._2
-        }.isEmpty) {
-      return false
-    }
-    if (adjacent.find { d =>
-          d._1 > d._2
-        }.nonEmpty) {
+    if (stream
+          .sliding(2)
+          .find { d =>
+            d(0) > d(1)
+          }
+          .nonEmpty) {
       return false
     }
     return true
--- day_4/src/test/scala/day4/PasswordSpec.scala
+++ day_4/src/test/scala/day4/PasswordSpec.scala
@@ -1,33 +1,59 @@
 package day4
 
 import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class PasswordSpec extends FlatSpec with Matchers {
+class PasswordSpec
+    extends FlatSpec
+    with Matchers
+    with TableDrivenPropertyChecks {
+  val booleans = Table("v", true, false)
+
   "The Password object" should "succeed with valid passwords" in {
+    forAll(booleans) { implicit part1 =>
+      Password.isValid("122345") shouldBe true
+      Password.isValid("112233") shouldBe true
+      Password.isValid("111122") shouldBe true
+    }
+
+    implicit val part1 = true
     Password.isValid("111111") shouldBe true
     Password.isValid("111123") shouldBe true
-    Password.isValid("122345") shouldBe true
+    Password.isValid("123444") shouldBe true
   }
 
   it should "fail with invalid passwords" in {
-    Password.isValid("135679") shouldBe false
-    Password.isValid("223450") shouldBe false
-    Password.isValid("123789") shouldBe false
-    Password.isValid("11111") shouldBe false
-    Password.isValid("1111111") shouldBe false
+    Password.isValid("111111") shouldBe false
+    Password.isValid("111123") shouldBe false
+    Password.isValid("123444") shouldBe false
+
+    forAll(booleans) { implicit part1 =>
+      Password.isValid("135679") shouldBe false
+      Password.isValid("223450") shouldBe false
+      Password.isValid("123789") shouldBe false
+      Password.isValid("11111") shouldBe false
+      Password.isValid("1111111") shouldBe false
+    }
   }
 
   it should "solve part 1" in {
+    implicit val part1 = true
     val start = 147981
     val end = 691423
     Stream
       .from(start)
       .take(end - start)
-      .map { n =>
-        n.toString()
-      }
-      .count { p =>
-        Password.isValid(p)
-      } shouldBe 1790
+      .map(_.toString)
+      .count(Password.isValid _) shouldBe 1790
+  }
+
+  it should "solve part 2" in {
+    val start = 147981
+    val end = 691423
+    Stream
+      .from(start)
+      .take(end - start)
+      .map(_.toString)
+      .count(Password.isValid _) shouldBe 1206
   }
 }
